a1e1717dc8fc50f31f510e77c44355ee6c6dd883
Aakash Bhandary <aakashbhandary2024@gmail.com>
2026-06-09 19:49:44 -0400

    Add: combination-target-sum-ii - submission-1

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-1.py
@@ -0,0 +1,30 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        # we can choose the same value, but not the same combination.
+        res = []
+
+        # we can sort and skip
+        candidates.sort()
+
+        def dfs(i, curr, total):
+            # the 'curr not in res' is giving a TLE
+            if total == target:
+                res.append(curr[:])
+                return
+            
+            if i >= len(candidates) or total > target:
+                return
+            
+
+            # include
+            curr.append(candidates[i])
+            dfs(i+1, curr, total + candidates[i])
+            curr.pop()
+
+            while i+1 < len(candidates) and candidates[i] == candidates[i+1]:
+                i += 1
+
+            dfs(i+1, curr, total)
+
+        dfs(0, [], 0)
+        return res
