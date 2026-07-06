4ea8a8ffad9f4819e974019f9f8279147cf23e2c
Aakash Bhandary <aakashbhandary2024@gmail.com>
2026-07-06 18:06:03 -0400

    Add: course-schedule - submission-3

---
+++ Data Structures & Algorithms/course-schedule/submission-3.py
@@ -0,0 +1,43 @@
+class Solution:
+    def getAdj(self, V, edges):
+        adj = [[] for _ in range(V)]
+        for edge in edges:
+            adj[edge[1]].append(edge[0]) # correct order of edges
+        
+        return adj
+
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # cycle detection using kahn's algorithm
+        adj = self.getAdj(numCourses, prerequisites)
+
+        # calculate indegree
+        indegree = [0] * numCourses
+        for i in range(numCourses):
+            for to in adj[i]:
+                indegree[to] += 1
+        
+        # BFS
+        q = deque()
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                q.append(i)
+        
+        idx = 0
+        # res = [0] * numCourses
+
+        while q:
+            curr = q.popleft()
+            # res[idx] = curr
+            idx += 1
+
+            for to in adj[curr]:
+                indegree[to] -= 1
+                if indegree[to] == 0:
+                    q.append(to)
+            
+        if idx != numCourses:
+            return False
+        return True
+
+
+
